693ed902b66e9e5bac1313d514d775dfafd1ab99
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-06-21 15:41:26 +0100

    fix home import issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import FeaturedDishes from "@/components/home/FeaturedDishes";
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <FeaturedDishes />
     </>
   );
 }
